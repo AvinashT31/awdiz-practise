2e4692bd51b74012c0ea0141ff2f5f935fc48b96
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-01-29 20:40:27 +0530

    work almost done

---
+++ javascript.js
@@ -0,0 +1,37 @@
+//Assignment operators :
+ //Additon operator
+ //subtraction
+ //multiply
+ //divided
+ //remainder
+ //exponentional 
+
+
+var num1 = 10;
+var num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+
+//Comparison operator
+//Equal (==)
+//not equal(!=)
+//strict equal(===)
+//greater than(>)
+//greater than equal to(>=)
+//less than (<)
+//less than equal to(<=)
+
+var num1 = 10;
+var num2 = 13;
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 === num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
